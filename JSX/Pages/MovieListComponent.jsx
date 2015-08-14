3f822cc950024284827cd5a25eab1fe864898832
Deepak Sharma <deepak.sharma.cse12@itbhu.ac.in>
2015-08-14 18:59:05 +0530

    first commit

---
+++ JSX/Pages/MovieListComponent.jsx
@@ -0,0 +1,12 @@
+var MovieListComponent = React.createClass({
+	render(){
+		return (
+			<div>
+				<h2>Deepak Sharma</h2>
+			</div>
+		)
+	}
+});
+
+React.render(<MovieListComponent/>, 
+	document.getElementById('content'));
